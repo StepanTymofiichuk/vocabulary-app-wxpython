2ff2cd8c198db8011d7a670a5eca1fd187c116a3
pavilion15 <tymofiichyk1995@gmail.com>
2024-05-17 14:46:04 +0300

    add stats functionality

--- main-italiano-parlato.py
+++ main-italiano-parlato.py
@@ -25,6 +25,7 @@ class MyFrame(wx.Frame):
         self.MenuBar()
         
         self.Search()
+        self.Stats()
         self.Database()
         self.Controls()
 
@@ -95,25 +96,80 @@ class MyFrame(wx.Frame):
     def Search(self):
             
         #panel1 = wx.Panel(self, size=(400,40), pos=(10,0))
-        search_box = wx.StaticBox(self, label="Search Functionality", size=(390,50), pos=(10,5), style=1)
-        search_text = wx.StaticText(search_box, label="Search", pos=(20,25))
+        search_box = wx.Panel(self, size=(390,50), pos=(10,0), style=1)
+        search_text = wx.StaticText(search_box, label="Cercare", pos=(16,25))
         font = wx.Font(wx.FontInfo(12).FaceName("Helvetica"))
         #search_text.SetFont(font)
         global search_textCtrl
-        search_textCtrl = wx.TextCtrl(search_box, value=str(len(rows)) + " words", pos=(60,18))
-        search_button = wx.Button(search_box, -1, label="Go", pos=(180,18))
-        refresh_button = wx.Button(search_box, label="Refresh", pos=(260,18))
+        search_textCtrl = wx.TextCtrl(search_box, value=str(len(rows)) + " parole e frasii", pos=(60,18))
+        search_button = wx.Button(search_box, -1, label="Va", pos=(180,18))
+        refresh_button = wx.Button(search_box, label="Ricaricare", pos=(260,18))
         self.Bind(wx.EVT_BUTTON, self.OnButton, id=search_button.GetId())
+
+    def Stats(self):
+        level1_query = "SELECT COUNT(word) FROM italiano_parlato WHERE studied<20"
+        level2_query = "SELECT COUNT(word) FROM italiano_parlato WHERE studied BETWEEN 20 AND 40"
+        level3_query = "SELECT COUNT(word) FROM italiano_parlato WHERE studied BETWEEN 60 AND 80"
+        level1_fr_query = "SELECT COUNT(word) FROM frasi_italiane_parlato WHERE studied<20"
+        level2_fr_query = "SELECT COUNT(word) FROM frasi_italiane_parlato WHERE studied BETWEEN 20 AND 40"
+        level3_fr_query = "SELECT COUNT(word) FROM frasi_italiane_parlato WHERE studied BETWEEN 60 AND 80"
+        c.execute(level1_query)
+        c1 = conn.cursor()
+        c2 = conn.cursor()
+        c1.execute(level2_query)
+        c2.execute(level3_query)
+        c1_fr = conn.cursor()
+        c2_fr = conn.cursor()
+        c3_fr = conn.cursor()
+        c1_fr.execute(level1_fr_query)
+        c2_fr.execute(level2_fr_query)
+        c3_fr.execute(level3_fr_query)
+        lvl1_result = c.fetchone()
+        lvl2_result = c1.fetchone()
+        lvl3_result = c2.fetchone()
+        lvl1_fr_result = c1_fr.fetchone()
+        lvl2_fr_result = c2_fr.fetchone()
+        lvl3_fr_result = c3_fr.fetchone()
+        print(lvl1_result)
+        print(lvl2_result)
+        print(lvl3_result)
+        print(lvl1_fr_result)
+        print(lvl2_fr_result)
+        print(lvl3_fr_result)
+        stats_box = wx.StaticBox(self, size=(390,76), pos=(10,50))
+        box_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        title = wx.StaticText(stats_box, label="Italiano parlato parole", pos=(15,10))
+        title_words_left = wx.StaticText(stats_box, label="Parole rimaste", pos=(160,28))
+        title1 = wx.StaticText(stats_box, label="Italiane parlate frasi", pos=(265,10))
+        font = wx.Font(wx.FontInfo(16).FaceName("Helvetica"))
+        level1_words = wx.StaticText(stats_box, label=str(lvl1_result[0]), pos=(20, 26))
+        level1_title = wx.StaticText(stats_box, label="Lvl 1", pos=(18, 50))
+        level1_words.SetFont(font)
+        level2_words = wx.StaticText(stats_box, label=str(lvl2_result[0]), pos=(50, 26))
+        level2_title = wx.StaticText(stats_box, label="Lvl 2", pos=(48, 50))
+        level2_words.SetFont(font)
+        level3_words = wx.StaticText(stats_box, label=str(lvl3_result[0]), pos=(90, 26))
+        level3_title = wx.StaticText(stats_box, label="Lvl 3", pos=(88, 50))
+        level3_words.SetFont(font)
+        level1_fr_words = wx.StaticText(stats_box, label=str(lvl1_fr_result[0]), pos=(275, 26))
+        level1_fr_title = wx.StaticText(stats_box, label="Lvl 1", pos=(273, 50))
+        level1_fr_words.SetFont(font)
+        level2_fr_words = wx.StaticText(stats_box, label=str(lvl2_fr_result[0]), pos=(305, 26))
+        level2_fr_title = wx.StaticText(stats_box, label="Lvl 2", pos=(303, 50))
+        level2_fr_words.SetFont(font)
+        level3_fr_words = wx.StaticText(stats_box, label=str(lvl3_fr_result[0]), pos=(345, 26))
+        level3_fr_title = wx.StaticText(stats_box, label="Lvl 3", pos=(343, 50))
+        level3_fr_words.SetFont(font)
 
     def Database(self):
 
         #panel = wx.Panel(self, size=(400,400), pos=(10,60))
-        db_box = wx.StaticBox(self, label="Databae Functionality", size=(390,390), pos=(10,60))
+        db_box = wx.Panel(self, size=(390,340), pos=(10,123))
         global db_listControl
-        db_listControl = wx.ListCtrl(db_box, style=wx.LC_REPORT, pos=(20,20), size=(350,350))
-        db_listControl.InsertColumn(0, "Word")
-        db_listControl.InsertColumn(1, "Translation")
-        db_listControl.InsertColumn(2, "Studied %")
+        db_listControl = wx.ListCtrl(db_box, style=wx.LC_REPORT, pos=(20,20), size=(350,305))
+        db_listControl.InsertColumn(0, "Parola")
+        db_listControl.InsertColumn(1, "Traduzione")
+        db_listControl.InsertColumn(2, "Studiato %")
         db_listControl.SetColumnWidth(0, 120)
         db_listControl.SetColumnWidth(1, 120)
         db_listControl.SetColumnWidth(2, 80)
@@ -122,13 +178,20 @@ class MyFrame(wx.Frame):
 
     def Controls(self):
 
-        controls_box = wx.StaticBox(self, label="Controls", size=(390,50), pos=(10,450))
+        controls_box = wx.Panel(self, size=(390,50), pos=(10,450))
         add_to_db_button = wx.Button(controls_box, -1, label="Inserire Al db", pos=(10,18))
+        clear_from_db_button = wx.Button(controls_box, -1, label="Azzerare db", pos=(103,18))
         self.Bind(wx.EVT_BUTTON, self.AddToDbButton, id=add_to_db_button.GetId())
+        self.Bind(wx.EVT_BUTTON, self.ClearDbButton, id=clear_from_db_button.GetId())
 
     def AddToDbButton(self, event):
         print("Clicked")
         script_file = "add.py"
+        Popen([python_bin, script_file])
+
+    def ClearDbButton(self, event):
+        print("Clicked")
+        script_file = "clear.py"
         Popen([python_bin, script_file])
 
     def OnButton(self, event):
